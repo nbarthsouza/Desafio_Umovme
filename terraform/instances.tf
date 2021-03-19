6e20ae0fe746fce87c94623c85e8dd385e9bb535
nbarthsouza <67973167+nbarthsouza@users.noreply.github.com>
2021-03-18 23:10:32 -0300

    Update instances.tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "prod" {
   }
   vpc_security_group_ids = [ aws_security_group.main-sg.id ]
   provisioner "local-exec" {
-    command = "sleep 20; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u '${var.ansible_user}' -i '${aws_instance.prod.public_ip},' --private-key '${var.key_locate}' '${var.playbook_locate}'"
+    command = "sleep 20; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u '${var.ansible_user}' -i '${aws_instance.prod.public_ip},' --private-key '${var.key_location}' '${var.playbook_location}'"
   }
 }
